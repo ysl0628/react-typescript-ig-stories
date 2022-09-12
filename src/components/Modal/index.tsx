831462bbd3cd812b4ef09e8b229ce96285e582ea
ysl0628 <yihsinlan@gmail.com>
2022-09-12 21:03:13 +0800

    style: slide position height

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -20,15 +20,29 @@ export default function Modal({ show, close }: ModalProps) {
         </button>
         <div className="slide-items">
           <img
-            src="https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1631&q=80"
+            src="https://images.unsplash.com/photo-1661347335435-6d851eb110f5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
             alt=""
           />
-          <img src="./img/img5.jpg" alt="" />
-          <img src="./img/img6.jpg" alt="" />
-          <img src="./img/img7.jpg" alt="" />
+          <img
+            src="https://images.unsplash.com/photo-1661347335435-6d851eb110f5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+            alt=""
+          />
+          <img
+            src="https://images.unsplash.com/photo-1661347335435-6d851eb110f5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+            alt=""
+          />
+          <img
+            src="https://images.unsplash.com/photo-1661347335435-6d851eb110f5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+            alt=""
+          />
         </div>
         <nav className="slide-nav">
-          <div className="slide-thumb"></div>
+          <div className="slide-thumb">
+            <span className="active"></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
           <button className="slide-prev">prev</button>
           <button className="slide-next">next</button>
         </nav>
