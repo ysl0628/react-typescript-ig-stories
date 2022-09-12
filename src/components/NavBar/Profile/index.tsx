bfa7b50db18a4d8a480edc9615108df949314a49
ysl0628 <yihsinlan@gmail.com>
2022-09-12 12:28:12 +0800

    feat: add modal toggle

--- src/components/NavBar/Profile/index.tsx
+++ src/components/NavBar/Profile/index.tsx
@@ -1,12 +1,24 @@
+import { useState } from "react";
+import Modal from "../../Modal";
+
 type ProfileProps = {
   id: string;
   username?: string;
   url: string;
 };
 export default function Profile(props: ProfileProps) {
+  const [modalShow, setModalShow] = useState<boolean>(false);
   return (
-    <div className="content">
-      <img src={props.url} alt="" />
-    </div>
+    <>
+      <div
+        className="content"
+        onClick={() => {
+          setModalShow((prev) => !prev);
+        }}
+      >
+        <img src={props.url} alt="" />
+      </div>
+      {modalShow && <Modal />}
+    </>
   );
 }
