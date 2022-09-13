37cc9c9b2ecc7c3fc368ecb222a736d4b7e113e8
ysl0628 <yihsinlan@gmail.com>
2022-09-13 22:53:43 +0800

    feat: implement auto slide function

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,3 +1,4 @@
+import { useCallback, useEffect, useState } from "react";
 import { useAppDispatch, useAppSelector } from "../../hooks/useRedux";
 import { init, nextSlide, prevSlide } from "../../store/reducers/slideSlice";
 import Backdrop from "../../UI/Backdrop";
@@ -13,17 +14,25 @@ export default function Modal({ show, close, posts }: ModalProps) {
   const currentSlideIndex = useAppSelector(
     (state) => state.slide.activeSlideIndex
   );
-  console.log(currentSlideIndex);
   const dispatch = useAppDispatch();
 
   const prevHandler = () => {
-    console.log("按了 prev 按鈕");
+    if (currentSlideIndex === 0) {
+      return dispatch(init());
+    }
     dispatch(prevSlide());
   };
-  const nextHandler = () => {
-    console.log("按了 next 按鈕");
-    dispatch(nextSlide());
-  };
+  const nextHandler = useCallback(() => {
+    if (currentSlideIndex < posts.length - 1) {
+      return dispatch(nextSlide());
+    }
+    dispatch(init());
+  }, [currentSlideIndex]);
+
+  useEffect(() => {
+    const timer = setTimeout(nextHandler, 5000);
+    return () => clearTimeout(timer);
+  }, [nextHandler]);
 
   return (
     <Backdrop>
@@ -58,18 +67,10 @@ export default function Modal({ show, close, posts }: ModalProps) {
               ></span>
             ))}
           </div>
-          <button
-            onClick={prevHandler}
-            disabled={currentSlideIndex === 0}
-            className="slide-prev"
-          >
+          <button onClick={prevHandler} className="slide-prev">
             prev
           </button>
-          <button
-            onClick={nextHandler}
-            disabled={currentSlideIndex === posts.length - 1}
-            className="slide-next"
-          >
+          <button onClick={nextHandler} className="slide-next">
             next
           </button>
         </nav>
