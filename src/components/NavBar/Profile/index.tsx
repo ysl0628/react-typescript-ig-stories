59911f952886f7135e6a2de7377de2a01976941d
ysl0628 <yihsinlan@gmail.com>
2022-09-15 22:07:57 +0800

    feat: remove useless code

--- src/components/NavBar/Profile/index.tsx
+++ src/components/NavBar/Profile/index.tsx
@@ -35,9 +35,6 @@ export default function Profile({
       >
         <img src={url} alt="" />
       </div>
-      {/* {modalShow && (
-        <Modal show={modalShow} close={setModalShow} posts={posts} />
-      )} */}
     </>
   );
 }
