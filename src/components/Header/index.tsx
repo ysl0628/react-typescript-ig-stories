2392641cc7a3424a94dc63f337a804de67399560
ysl0628 <yihsinlan@gmail.com>
2022-10-23 22:56:11 +0800

    feat: add ig logo on Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import "./index.css";
+
+const index = () => {
+  return (
+    <div className="header">
+      <div className="headerImage">
+        <img
+          src="https://www.freepnglogos.com/uploads/logo-ig-png/logo-ig-instagram-new-logo-vector-download-5.png"
+          width="30"
+          alt="logo ig, instagram new logo vector download"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default index;
