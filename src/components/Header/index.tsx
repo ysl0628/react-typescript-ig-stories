3f430818ff9c47704d77cf70d1508cf34cff00bc
ysl0628 <yihsinlan@gmail.com>
2022-12-03 00:06:17 +0800

    feat: implement sign up and sign in button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,26 +1,58 @@
-import { useState } from "react";
-import "./index.css";
-import SignUp from "./SignUp/SignUp";
+import { User } from 'firebase/auth'
+import { useState } from 'react'
+import { auth } from '../../firebase'
+import './index.css'
+import SignIn from './SignIn/SignIn'
+import SignUp from './SignUp/SignUp'
 
 const Header = () => {
-  const [signUpOpen, setSignUpOpen] = useState<boolean>(false);
-  const handleClick = () => setSignUpOpen(true);
-  const handleClose = () => setSignUpOpen(false);
+  const [signUpOpen, setSignUpOpen] = useState<boolean>(false)
+  const [signInOpen, setSignInOpen] = useState<boolean>(false)
+  const [user, setUser] = useState<User | null>(null)
+
+  const handleSignUpOpen = () => setSignUpOpen(true)
+  const handleSignUpClose = () => setSignUpOpen(false)
+  const handleSignInOpen = () => setSignInOpen(true)
+  const handleSignInClose = () => setSignInOpen(false)
+  const handleSignOut = () => {
+    auth.signOut()
+    setUser(null)
+  }
+  console.log(user)
+
   return (
-    <div className="header">
-      <div className="headerImage">
+    <div className='header'>
+      <div className='headerImage'>
         <img
-          src="https://www.freepnglogos.com/uploads/logo-ig-png/logo-ig-instagram-new-logo-vector-download-5.png"
-          width="30"
-          alt="logo ig, instagram new logo vector download"
+          src='https://www.freepnglogos.com/uploads/logo-ig-png/logo-ig-instagram-new-logo-vector-download-5.png'
+          width='30'
+          alt='logo ig, instagram new logo vector download'
         />
       </div>
-      <div className="signUp-btn">
-        <button onClick={handleClick}>Sign up</button>
-        {signUpOpen && <SignUp handleClose={handleClose} />}
+      <div className='signUp-btn'>
+        {user ? (
+          <button onClick={handleSignOut}>Logout</button>
+        ) : (
+          <button onClick={handleSignUpOpen}>Sign up</button>
+        )}
+        {signUpOpen && (
+          <SignUp
+            setUser={setUser}
+            user={user}
+            handleClose={handleSignUpClose}
+          />
+        )}
+        {!user && <button onClick={handleSignInOpen}>Sign in</button>}
+        {signInOpen && (
+          <SignIn
+            setUser={setUser}
+            user={user}
+            handleClose={handleSignInClose}
+          />
+        )}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
